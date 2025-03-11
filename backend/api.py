1a2ce67e0c731c8e2dc78fa3258be70c8f877910
Swetharajadurai <swetha@leadtap.ai>
2025-03-11 10:43:06 +0530

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -12,9 +12,18 @@ app = Flask(__name__)
 CORS(app)
 
 SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
+AUTHORIZED_BEARER_TOKEN =os.getenv("BEARER_TOKEN")
 
 @app.route("/send-email", methods=["POST"])
 def send_email():
+    auth_header = request.headers.get("Authorization")
+    if not auth_header or not auth_header.startswith("Bearer "):
+        return jsonify({"error": "Unauthorized"}), 401
+    
+    token = auth_header.split("Bearer ")[1]
+    if token != AUTHORIZED_BEARER_TOKEN:
+        return jsonify({"error": "Invalid token"}), 403
+
     data = request.json
     name = data.get("name")
     email = data.get("email")
